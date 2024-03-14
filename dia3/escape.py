d3477aeaf52f089752437322f8a8a89a96600437
traipe <jimenatraipe@gmail.com>
2024-03-14 11:28:01 -0300

    se agrego mas info a velocidad de escape

--- dia3/escape.py
+++ dia3/escape.py
@@ -1,17 +1,19 @@
-#import math
+#import math solo el necesario a utilizar, en este caso raíz cuadrada
 from math import sqrt
 
-#datos de entrada
+#datos de entrada 
 print("ingrese el radio en kilometros:")
+#radio
 r = float(input(""))
 print("ingrese la constante g:")
+#gravedad
 g = float(input(""))
 
-#convertir el radio a metros
+#convertir el radio que estaba a km a metros, multiplicando radio por 1000
 r = r * 1000
 
-#calcular la velocidad de escape
+#calcular la velocidad de escape, multiplicando raiz x los elementos
 ve = sqrt(2 * r * g)
 
-#Mostrar la velocidad de escape
+#Mostrar la velocidad de escape con texto 
 print(f"La velocidad de Escape es {ve} [m/s]")

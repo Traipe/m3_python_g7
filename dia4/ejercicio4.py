db82efa05e85b36e9c61893ee213c7c692dad979
traipe <jimenatraipe@gmail.com>
2024-03-15 13:08:34 -0300

    ejercicio suma y pseudocodigo

---
+++ dia4/ejercicio4.py
@@ -0,0 +1,21 @@
+""" 
+Sumar dos numeros enteros ingresados por el usuario
+mostrar el resultado de la suma
+inicio
+leer numero1
+leer numero2
+numero1 + numero2
+mostrar numero1 + numero2
+fin
+"""
+numero1 = int(input("ingrese el primero numero"))
+numero2 = int(input("ingrese el segundo numero"))
+
+#suma = numero1 + numero2 
+
+print(f"El resultado de la suma es: {numero1 + numero2}")
+
+base = float(input("ingresa la base del rectangulo"))
+altura = float(input("ingrese la altura del rectangulo"))
+print(f"El area de un rectanfulo es {base*altura}")
+

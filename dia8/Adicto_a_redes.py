3d6d18013b028c5b9cecddd2c69d61ba68fbab28
traipe <jimenatraipe@gmail.com>
2024-03-21 13:09:39 -0300

    agregue informacion como comentario para: adicto a redes y ventas

---
+++ dia8/Adicto_a_redes.py
@@ -0,0 +1,10 @@
+""" 
+Se tiene una lista con la cantidad de minutos usados en redes sociales de distintos
+usuarios.
+Se debe retornar una lista que clasifique todos los tiempos menores a 90 minutos
+como 'bien' y todos los tiempos mayores o iguales a 90 como 'mal'.
+El output debería ser algo similar a lo siguiente:
+[120, 50, 600, 30, 90, 10, 200, 0, 500]
+['mal', 'bien', 'mal', 'bien', 'bien', 'bien', 'mal', 'bien', 'mal']
+
+"""
---
+++ dia8/Ventas.py
@@ -0,0 +1,13 @@
+""" 
+Dada la información de ventas de 3 meses:
+
+Mes              Ventas
+Octubre          65000
+Noviembre        68000
+Diciembre        72000
+
+1. Convertir la tabla en un diccionario
+2. Modificar el diccionario para incrementar las ventas en un 10%.
+3. Hacer un nuevo diccionario pero con las ventas disminuidas un 20%.
+
+"""

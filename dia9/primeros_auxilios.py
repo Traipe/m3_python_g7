004a0f9aeabc998954b49a21cdbc15cde1fd4ffb
traipe <jimenatraipe@gmail.com>
2024-03-22 12:04:33 -0300

    agregue el codigo anterior, que funciona, pero que corregiremos despues de clases

--- dia9/primeros_auxilios.py
+++ dia9/primeros_auxilios.py
@@ -4,33 +4,33 @@ Najla Gatica
 Lolett Llanquinao
 Jimena Traipe
 """
-def primeros_auxilios():
-    print("¡Bienvenido a la aplicación de primeros auxilios!")
-    print("Por favor, responde las siguientes preguntas para obtener ayuda:")
-
-    
-respuesta = input("¿La persona necesita atención médica inmediata? (si/no): ").lower()
-if respuesta == "si":
-            print("Llevar a la persona al hospital de inmediato.")
-            print("Fin.")
-            
+#Primeros Auxilios
+print("¿Necesita ayuda? responda estas preguntas para ayudarle en su problema\n")
+#usuario ingresa opción
+opcion_usuario = input("¿La persona responde a estímulos? (si/no):\n")
+print(opcion_usuario)
+if  opcion_usuario.lower() == "si":
+    print("Diríjase al hospital más cercano")
+elif opcion_usuario.lower() == "no":
+    print("Verificar que las vías aéreas no estén obstruidas\n")
 else:
-            print("Abre la vía aérea.")
+    print("FIN se murió")
 
-            respuesta = input("¿La persona está respirando? (si/no): ").lower()
-            if respuesta == "si":
-                print("Permite la ventilación.")
-            else:
-                print("Administra 5 ventilaciones y llama a la ambulancia.")
-while True:
-            respuesta = input("¿La persona tiene signos de vida? (si/no): ").lower()
-            if respuesta == "si":
-                print("Espera a la ambulancia.")
-                respuesta = input("¿Ha llegado la ambulancia? (si/no): ").lower()
-                if respuesta == "si":
-                    print("Fin.")
-                    break
-            else:
-                print("Realiza compresiones torácicas hasta que llegue la ambulancia.")
 
-primeros_auxilios()
+opcion_usuario = input("¿Está respirando? (si/no):\n")
+if opcion_usuario.lower() == "si":
+    print("Darle espacio a la persona para que pueda respirar")
+elif opcion_usuario.lower() == "no":
+    print("Administrar 5 ventilaciones y llamar a la ambulancia")
+
+
+    opcion_usuario = input("¿Signos de vida? (si/no):\n")
+    if opcion_usuario.lower() == "si":
+        print("Reevaluar signos vitales a la espera de la ambulancia")
+    elif opcion_usuario.lower() == "no":
+        print("Administrar compresiones torácicas hasta que llegue la ambulancia")
+
+while True: #aqui va el loop
+    opcion_usuario = input("¿Llegó la ambulancia? (si/no):\n")
+    if opcion_usuario.lower() == "si":
+        print("FIN del programa")
